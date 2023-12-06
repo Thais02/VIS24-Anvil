88749d8eb216c83a44bbd56d89e36b6c0e8d2183
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-06 00:11:45 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import plotly.graph_objects as go
 
 class Form1(Form1Template):
     def __init__(self, **properties):
@@ -7,3 +8,10 @@ class Form1(Form1Template):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def type_button_click(self, sender, **event_args):
+        self.button_vs.role = ''
+        self.button_poule.role = ''
+        self.button_hist.role = ''
+        sender.role = 'primary-color'
+        self.refresh_data_bindings()
