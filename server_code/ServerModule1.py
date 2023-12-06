957dc6fca0aae9e03813b92186fdf42a5760a761
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-06 14:26:34 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -5,7 +5,7 @@ import pandas as pd
 def get_data():
     data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_with_codes.csv')
     print(data.head())
-    data.drop_duplicates(subset=['iso_alpha'], keep='first', inplace=True)
+    data.drop_duplicates(subset=['iso_alpha'], keep='last', inplace=True)
     return list(data['iso_alpha']), list(data['gdpPercap'])
 
 
