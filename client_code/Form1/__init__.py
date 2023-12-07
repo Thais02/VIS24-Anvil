63cea6dae52ac6e202269a2e3b7903b36284ad96
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-07 11:09:47 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,17 +9,17 @@ class Form1(Form1Template):
         self.init_components(**properties)
 
         # self.plot_1.data = go.Bar(x=list(range(2, 40, 2)), y=list(range(2, 40, 2)), name='Example interactive plot')
-        locs, z = anvil.server.call('get_data')
-        self.plot_map.data = go.Choropleth(locations=locs, z=z,
-                    # projection='natural earth',
-                    title='GDP per Capita by Country',
+        locs, z, countries = anvil.server.call('get_data')
+        self.plot_map.data = go.Choropleth(locations=locs, z=z, text=countries,
+                    title='Total amount of goals scored per country',
                     colorscale = 'Blues',
                     autocolorscale=False,
                     reversescale=True,
                     marker_line_color='darkgray',
                     marker_line_width=0.5,
-                    colorbar_tickprefix = '$',
-                    colorbar_title = 'GDP<br>Billions US$')
+                    # colorbar_tickprefix = '$',
+                    colorbar_title = 'Goals scored')
+        self.plot_map.layout.geo = {'showframe': False, 'showcoastlines': False, 'projection_type': 'equirectangular'}
         # Any code you write here will run before the form opens.
 
     def type_button_click(self, sender, **event_args):
@@ -27,4 +27,7 @@ class Form1(Form1Template):
         self.button_poule.role = ''
         self.button_hist.role = ''
         sender.role = 'primary-color'
+        self.refresh_data_bindings()
+
+    def slider_1_change(self, level, **event_args):
         self.refresh_data_bindings()
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -4,8 +4,8 @@ import pandas as pd
 @anvil.server.callable
 def get_data():
     data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_with_codes.csv')
-    print(data.head())
+    print(data.columns)
     data.drop_duplicates(subset=['iso_alpha'], keep='last', inplace=True)
-    return list(data['iso_alpha']), list(data['gdpPercap'])
+    return list(data['iso_alpha']), list(data['gdpPercap']), list(data['country'])
 
 
