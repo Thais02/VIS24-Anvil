0e1a07767ffa69aea64b6880a98473b4a107a17c
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-07 16:36:36 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,39 +10,40 @@ class Form1(Form1Template):
         # Any code you write here will run before the form opens.
 
     def refresh_map(self):
-        isos, goals, countries, top5 = self.data[str(int(self.slider_1.level))]
+        isos, goals, countries, top5 = self.data[str(int(self.slider.value))]
         top5_x = list(top5.values())
         top5_x.reverse()
         top5_y = list(top5.keys())
         top5_y.reverse()
         map = go.Choropleth(locations=isos, z=goals, text=countries,
-                    title = 'Total amount of goals scored per country',
                     colorscale = 'Reds',
                     autocolorscale = True,
                     reversescale = False,
                     marker_line_color = 'darkgray',
                     marker_line_width = 0.5,
                     colorbar_title = 'Goals scored')
-        bars = go.Bar(x=top5_x, y=top5_y, name='Top 5', orientation='h', marker={
-            'color': top5_x,
-            'colorscale': 'Reds',
-            'cmin': min(goals),
-            'cmax': max(goals),
+        bars = go.Bar(x=top5_x, y=top5_y,
+                      orientation='h',
+                      marker={
+                        'color': top5_x,
+                        'colorscale': 'Reds',
+                        'cmin': min(goals),
+                        'cmax': max(goals),
         })
+        
         self.plot_map.data = map
         self.plot_map.layout.geo = {'showframe': False, 'showcoastlines': False, 'projection_type': 'equirectangular'}
-        self.plot_bar.data = bars
+        self.plot_map.layout.title = 'Total amount of goals scored per country'
         
-
-    def slider_1_change(self, **event_args):
-        self.refresh_data_bindings()
-        self.refresh_map()
+        self.plot_bar.data = bars
+        self.plot_bar.layout.title = 'Top 5'
 
     def form_show(self, **event_args):
         with Notification('Fetching data...', title='Please wait'):
             self.data = anvil.server.call('get_data')
             self.refresh_map()
             self.plot_map.redraw()
+            self.plot_bar.redraw()
 
     def button_play_click(self, **event_args):
         if self.button_play.icon == 'fa:play':
@@ -53,11 +54,15 @@ class Form1(Form1Template):
             self.button_play.icon = 'fa:play'
 
     def timer_tick(self, **event_args):
-        if self.slider_1.level + 4 == 1942 or self.slider_1.level + 4 == 1946:
-            self.slider_1.level = 1950
-        elif self.slider_1.level + 4 > 2022:
-            self.slider_1.level = 1930
+        if self.slider.value + 4 == 1942 or self.slider.value + 4 == 1946:
+            self.slider.value = 1950
+        elif self.slider.value + 4 > 2022:
+            self.slider.value = 1930
         else:
-            self.slider_1.level += 4
-        self.slider_1_change()
-            
+            self.slider.value += 4
+        self.slider_change(None)
+
+    def slider_change(self, handle, **event_args):
+        self.refresh_data_bindings()
+        self.refresh_map()
+        
