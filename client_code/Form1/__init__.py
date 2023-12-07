63b0f6ff2c7ca608eb6199caee2bb1b05f822948
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-07 15:56:08 +0000

    Created matches_1930_2022.csv; Edited ServerModule1, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,27 +7,57 @@ class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
-        self.plot_bar.data = go.Bar(x=[20, 15, 10, 3], y=['USA', 'UK', 'Germany', 'Brazil'], name='Top 5', orientation='h')
-        locs, z, countries = anvil.server.call('get_data')
-        self.plot_map.data = go.Choropleth(locations=locs, z=z, text=countries,
-                    title='Total amount of goals scored per country',
-                    colorscale = 'Reds',
-                    autocolorscale=False,
-                    reversescale=False,
-                    marker_line_color='darkgray',
-                    marker_line_width=0.5,
-                    # colorbar_tickprefix = '$',
-                    colorbar_title = 'Goals scored')
-        self.plot_map.layout.geo = {'showframe': False, 'showcoastlines': False, 'projection_type': 'equirectangular'}
         # Any code you write here will run before the form opens.
 
-    def type_button_click(self, sender, **event_args):
-        self.button_vs.role = ''
-        self.button_poule.role = ''
-        self.button_hist.role = ''
-        sender.role = 'primary-color'
-        self.refresh_data_bindings()
+    def refresh_map(self):
+        isos, goals, countries, top5 = self.data[str(int(self.slider_1.level))]
+        top5_x = list(top5.values())
+        top5_x.reverse()
+        top5_y = list(top5.keys())
+        top5_y.reverse()
+        map = go.Choropleth(locations=isos, z=goals, text=countries,
+                    title = 'Total amount of goals scored per country',
+                    colorscale = 'Reds',
+                    autocolorscale = True,
+                    reversescale = False,
+                    marker_line_color = 'darkgray',
+                    marker_line_width = 0.5,
+                    colorbar_title = 'Goals scored')
+        bars = go.Bar(x=top5_x, y=top5_y, name='Top 5', orientation='h', marker={
+            'color': top5_x,
+            'colorscale': 'Reds',
+            'cmin': min(goals),
+            'cmax': max(goals),
+        })
+        self.plot_map.data = map
+        self.plot_map.layout.geo = {'showframe': False, 'showcoastlines': False, 'projection_type': 'equirectangular'}
+        self.plot_bar.data = bars
+        
 
-    def slider_1_change(self, level, **event_args):
+    def slider_1_change(self, **event_args):
         self.refresh_data_bindings()
+        self.refresh_map()
+
+    def form_show(self, **event_args):
+        with Notification('Fetching data...', title='Please wait'):
+            self.data = anvil.server.call('get_data')
+            self.refresh_map()
+            self.plot_map.redraw()
+
+    def button_play_click(self, **event_args):
+        if self.button_play.icon == 'fa:play':
+            self.timer.interval = 1
+            self.button_play.icon = 'fa:pause'
+        else:
+            self.timer.interval = 0
+            self.button_play.icon = 'fa:play'
+
+    def timer_tick(self, **event_args):
+        if self.slider_1.level + 4 == 1942 or self.slider_1.level + 4 == 1946:
+            self.slider_1.level = 1950
+        elif self.slider_1.level + 4 > 2022:
+            self.slider_1.level = 1930
+        else:
+            self.slider_1.level += 4
+        self.slider_1_change()
+            
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,11 +1,80 @@
 import anvil.server
+
 import pandas as pd
+from collections import Counter
+
+import pycountry
+
+def get_iso(name):
+    isos = {
+    'Russia': 'RUS',
+    'IR Iran': 'IRN',
+    'Korea Republic': 'KOR',  # south korea
+    'Korea DPR': 'PRK',  # best korea
+    'Czech Republic': 'CZE',
+    'Türkiye': 'TUR',
+    'Republic of Ireland': 'IRL',
+    'China PR': 'CHN',
+    'Bolivia': 'BOL',
+    'Zaire': 'COD',  # Democratic Congo
+    'Dutch East Indies': 'IDN',  # Indonesia
+    # All English teams are combined as Great Britain
+    'England': 'GBR',
+    'Wales': 'GBR',
+    'Scotland': 'GBR',
+    'Northern Ireland': 'GBR',
+    # Both halves of Germany are combined
+    'West Germany': 'DEU',
+    'Germany DR': 'DEU',
+    # Montenegro is tiny so treat it as just Serbia
+    'Serbia and Montenegro': 'SRB',
+    # Fell apart in many countries but former capital became Serbia
+    'FR Yugoslavia': 'SRB',  # Serbia
+    'Yugoslavia': 'SRB',  # Serbia
+    # Idem with this former country
+    'Czechoslovakia': 'CZE',  # Czech Republic
+    # Soviet Union football team has been combined with Russia
+    'Soviet Union': 'RUS',
+    }
+    try:
+        iso = pycountry.countries.get(name=name).alpha_3
+    except:
+        iso = isos.get(name)
+    return iso
+
 
 @anvil.server.callable
 def get_data():
-    data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_with_codes.csv')
-    print(data.columns)
-    data.drop_duplicates(subset=['iso_alpha'], keep='last', inplace=True)
-    return list(data['iso_alpha']), list(data['gdpPercap']), list(data['country'])
-
-
+    df_full = pd.read_csv('https://vis.thijsblom.xyz/_/theme/matches_1930_2022.csv')
+    
+    data = {}  # {year: (country_iso's, goals, country_names)}
+    
+    for year in range(1930, 2022+1, 4):
+        df = df_full[df_full['Date'].str.contains(str(year))]
+    
+        goals = {}  # {country_iso: goals}
+    
+        for row in df.iterrows():
+            iso = get_iso(row[1]['home_team'])
+            goals[iso] = goals.get(iso, 0) + row[1]['home_score']
+            iso = get_iso(row[1]['away_team'])
+            goals[iso] = goals.get(iso, 0) + row[1]['away_score']
+    
+        list_iso = []
+        list_goals = []
+        list_countries = []
+        for iso, num_goals in goals.items():
+            list_iso.append(iso)
+            list_goals.append(num_goals)
+            list_countries.append(pycountry.countries.get(alpha_3=iso).name)
+        for country in pycountry.countries:
+            list_iso.append(country.alpha_3)
+            list_goals.append(goals.get(country.alpha_3, 0))
+            list_countries.append(country.name)
+        top5 = {}
+        for iso, goals in Counter(goals).most_common(5):
+            top5[pycountry.countries.get(alpha_3=iso).name] = goals
+    
+        data[str(year)] = (list_iso, list_goals, list_countries, top5)
+    
+    return data
