76b7523f750d5b999fa269c7603e77c15679f5c0
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-08 21:39:45 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -3,6 +3,8 @@ from anvil import *
 import anvil.server
 import plotly.graph_objects as go
 
+from collections import Counter
+
 class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
@@ -10,15 +12,34 @@ class Form1(Form1Template):
         # Any code you write here will run before the form opens.
 
     def refresh_map(self):
-        isos, goals, countries, top5 = self.data[str(int(self.slider_single.value))]
+        if self.checkbox_multiselect.checked:
+            data = {}
+            iso_to_name = {}
+            for year in range(int(self.slider_multi.values[0]), int(self.slider_multi.values[1])+1, 4):
+                isos, nums, countries, top5 = self.data[str(year)]
+                for iso, num, country in zip(isos, nums, countries):
+                    lst = data.get(iso, [])
+                    lst.append(num)
+                    data[iso] = lst
+                    iso_to_name[iso] = country
+            isos = list(data.keys())
+            nums = [sum(val)/len(val) for val in data.values()]
+            countries = [iso_to_name[iso] for iso in data.keys()]
+            top5 = {}
+            for iso, num in Counter({iso: num for iso, num in zip(isos, nums)}).most_common(5):
+                top5[iso_to_name[iso]] = num
+        else:
+            isos, nums, countries, top5 = self.data[str(int(self.slider_single.value))]
+        
         top5_x = list(top5.values())
         top5_x.reverse()
         top5_y = list(top5.keys())
         top5_y.reverse()
-        map = go.Choropleth(locations=isos, z=goals, text=countries,
-                    colorscale = 'Reds',
-                    autocolorscale = True,
-                    reversescale = True,
+        
+        map = go.Choropleth(locations=isos, z=nums, text=countries,
+                    colorscale = self.colorscale.text,
+                    autocolorscale = self.autocolorscale.checked,
+                    reversescale = self.reversescale.checked,
                     marker_line_color = 'darkgray',
                     marker_line_width = 0.5,
                     colorbar_title = 'Goals scored')
@@ -26,9 +47,10 @@ class Form1(Form1Template):
                       orientation='h',
                       marker={
                         'color': top5_x,
-                        'colorscale': 'Reds',
-                        'cmin': min(goals),
-                        'cmax': max(goals),
+                        'colorscale': self.colorscale.text,
+                        'reversescale': self.reversescale.checked,
+                        'cmin': min(nums),
+                        'cmax': max(nums),
         })
         
         self.plot_map.data = map
@@ -39,6 +61,7 @@ class Form1(Form1Template):
         self.plot_bar.layout.title = 'Top 5'
 
     def form_show(self, **event_args):
+        self.call_js('hideSidebar')
         with Notification('Fetching data...', title='Please wait'):
             try:
                 anvil.server.call('ping_uplink')
@@ -54,11 +77,15 @@ class Form1(Form1Template):
 
     def button_play_click(self, **event_args):
         if self.button_play.icon == 'fa:play':
+            self.checkbox_multiselect.enabled = False
             self.timer.interval = 1
             self.button_play.icon = 'fa:pause'
+            self.checkbox_multiselect.tooltip = 'Not available during animation'
         else:
             self.timer.interval = 0
             self.button_play.icon = 'fa:play'
+            self.checkbox_multiselect.enabled = True
+            self.checkbox_multiselect.tooltip = ''
 
     def timer_tick(self, **event_args):
         if self.slider_single.value + 4 == 1942 or self.slider_single.value + 4 == 1946:
@@ -75,11 +102,19 @@ class Form1(Form1Template):
 
     def checkbox_multiselect_change(self, **event_args):
         if self.checkbox_multiselect.checked:
+            self.button_play.enabled = False
             self.slider_multi.values = [self.slider_single.value, min(2022, self.slider_single.value+4)]
             self.slider_single.visible = False
             self.slider_multi.visible = True
+            self.button_play.tooltip = 'Not available in multi-select mode'
         else:
             self.slider_single.value = self.slider_multi.value
             self.slider_multi.visible = False
             self.slider_single.visible = True
-            
+            self.button_play.enabled = True
+            self.button_play.tooltip = ''
+        self.slider_single_change(None)
+
+    def debug_setting_change(self, **event_args):
+        self.refresh_data_bindings()
+        self.refresh_map()
