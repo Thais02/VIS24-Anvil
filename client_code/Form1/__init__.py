48c68f44f113d2e8ff8217fb26df8c21463e6b7d
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2023-12-09 14:46:39 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,8 +9,43 @@ class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        self.data = {}
+        self.cmin = 99999
+        self.cmax = -99999
+        self.config = {
+            'colorscale': 'Reds',
+            'reversescale': False,
+            'colorbar_title': 'Goals scored',
+            'plot_map_layout_title': 'Total amount of goals scored per country',
+            'plot_bar_layout_title': 'Top 5'
+        }
         # Any code you write here will run before the form opens.
 
+    def get_data(self):
+        self.call_js('hideSidebar')
+        self.slider_single.enabled = False
+        self.checkbox_multiselect.enabled = False
+        self.button_play.enabled = False
+        self.cmin = 99999
+        self.cmax = -99999
+        with Notification('Fetching data...', title='Please wait'):
+            try:
+                anvil.server.call('ping_uplink')
+            except:
+                self.data, config = anvil.server.call('get_data', vis_name=self.radio_goals.get_group_value())
+            else:
+                self.data, config = anvil.server.call('get_data_uplink', vis_name=self.radio_goals.get_group_value())
+                Notification('Retrieved data from connected local source', title='Data fetched', style='success', timeout=6).show()
+            if config:
+                self.config = config
+            for isos, nums, countries, top5 in self.data.values():
+                self.cmin = min(self.cmin, min(nums))
+                self.cmax = max(self.cmax, max(nums))
+        self.slider_single.enabled = True
+        self.checkbox_multiselect.enabled = True
+        self.button_play.enabled = True
+        self.refresh_map()
+    
     def refresh_map(self):
         if self.checkbox_multiselect.checked:
             data = {}
@@ -35,45 +70,38 @@ class Form1(Form1Template):
         top5_x.reverse()
         top5_y = list(top5.keys())
         top5_y.reverse()
-        
+
         map = go.Choropleth(locations=isos, z=nums, text=countries,
-                    colorscale = self.colorscale.text,
-                    autocolorscale = self.autocolorscale.checked,
-                    reversescale = self.reversescale.checked,
+                    colorscale = self.config.get('colorscale', 'Reds'),
+                    autocolorscale = False,
+                    reversescale = self.config.get('reversescale', False),
                     marker_line_color = 'darkgray',
                     marker_line_width = 0.5,
-                    colorbar_title = 'Goals scored')
+                    zmin = self.cmin,
+                    zmax = self.cmax,
+                    colorbar_title = self.config.get('colorbar_title'))
         bars = go.Bar(x=top5_x, y=top5_y,
                       orientation='h',
                       marker={
                         'color': top5_x,
-                        'colorscale': self.colorscale.text,
-                        'reversescale': self.reversescale.checked,
-                        'cmin': min(nums),
-                        'cmax': max(nums),
+                        'colorscale': self.config.get('colorscale', 'Reds'),
+                        'reversescale': self.config.get('reversescale', False),
+                        'cmin': self.cmin,
+                        'cmax': self.cmax,
         })
         
-        self.plot_map.data = map
         self.plot_map.layout.geo = {'showframe': False, 'showcoastlines': False, 'projection_type': 'equirectangular'}
-        self.plot_map.layout.title = 'Total amount of goals scored per country'
+        self.plot_map.layout.title = self.config.get('plot_map_layout_title', '[untitled]')
+        self.plot_map.layout.title += f'<br>Average between {self.slider_multi.values[0]} and {self.slider_multi.values[1]}' if self.checkbox_multiselect.checked else ''
+        self.plot_map.data = map
         
+        self.plot_bar.layout.title = self.config.get('plot_bar_layout_title', '[untitled]')
         self.plot_bar.data = bars
-        self.plot_bar.layout.title = 'Top 5'
-
+    
     def form_show(self, **event_args):
         self.call_js('hideSidebar')
-        with Notification('Fetching data...', title='Please wait'):
-            try:
-                anvil.server.call('ping_uplink')
-            except:
-                self.data = anvil.server.call('get_data')
-            else:
-                self.data = anvil.server.call('get_data_uplink')
-                Notification('Retrieved data from connected local source', title='Data fetched', style='success', timeout=6).show()
-                
-        self.refresh_map()
-        self.plot_map.redraw()
-        self.plot_bar.redraw()
+
+        self.get_data()
 
     def button_play_click(self, **event_args):
         if self.button_play.icon == 'fa:play':
@@ -116,5 +144,10 @@ class Form1(Form1Template):
         self.slider_single_change(None)
 
     def debug_setting_change(self, **event_args):
+        self.config['reversescale'] = self.reversescale.checked
+        self.config['colorscale'] = self.colorscale.text
         self.refresh_data_bindings()
         self.refresh_map()
+
+    def radio_change(self, **event_args):
+        self.get_data()
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -44,44 +44,47 @@ def get_iso(name):
 
 
 @anvil.server.callable
-def get_data():
-    blacklist = ['ATA']
-    df_full = pd.read_csv('https://vis.thijsblom.xyz/_/theme/matches_1930_2022.csv')
-
-    # dictionary with years as keys. For each year has a tuple of:
-    # - list of the iso alpha_3 strings of the ALL countries (countries who did not participate/score get a value of 0)
-    # - list of the amount of goals made
-    # - list of the full country names, retrieved from pycountry for consistency
-    # - dictionary with country names as keys and amount of goals as values, for the top-5 only
-    data = {}  # {year: (country_iso's, goals, country_names, {country_name: goals})}
+def get_data(vis_name):
+    if vis_name == 'goals':
+        blacklist = ['ATA']
+        df_full = pd.read_csv('https://vis.thijsblom.xyz/_/theme/matches_1930_2022.csv')
     
-    for year in range(1930, 2022+1, 4):
-        df = df_full[df_full['Date'].str.contains(str(year))]
-    
-        goals = {}  # {country_iso: goals}
-    
-        for row in df.iterrows():
-            iso = get_iso(row[1]['home_team'])
-            goals[iso] = goals.get(iso, 0) + row[1]['home_score']
-            iso = get_iso(row[1]['away_team'])
-            goals[iso] = goals.get(iso, 0) + row[1]['away_score']
-    
-        list_iso = []
-        list_goals = []
-        list_countries = []
-        for iso, num_goals in goals.items():
-            list_iso.append(iso)
-            list_goals.append(num_goals)
-            list_countries.append(pycountry.countries.get(alpha_3=iso).name)
-        for country in pycountry.countries:
-            if country.alpha_3 not in blacklist:
-                list_iso.append(country.alpha_3)
-                list_goals.append(goals.get(country.alpha_3, 0))
-                list_countries.append(country.name)
-        top5 = {}
-        for iso, goals in Counter(goals).most_common(5):
-            top5[pycountry.countries.get(alpha_3=iso).name] = goals
-    
-        data[str(year)] = (list_iso, list_goals, list_countries, top5)
-    
-    return data
+        # dictionary with years as keys. For each year has a tuple of:
+        # - list of the iso alpha_3 strings of the ALL countries (countries who did not participate/score get a value of 0)
+        # - list of the amount of goals made
+        # - list of the full country names, retrieved from pycountry for consistency
+        # - dictionary with country names as keys and amount of goals as values, for the top-5 only
+        data = {}  # {year: (country_iso's, goals, country_names, {country_name: goals})}
+        
+        for year in range(1930, 2022+1, 4):
+            df = df_full[df_full['Date'].str.contains(str(year))]
+        
+            goals = {}  # {country_iso: goals}
+        
+            for row in df.iterrows():
+                iso = get_iso(row[1]['home_team'])
+                goals[iso] = goals.get(iso, 0) + row[1]['home_score']
+                iso = get_iso(row[1]['away_team'])
+                goals[iso] = goals.get(iso, 0) + row[1]['away_score']
+        
+            list_iso = []
+            list_goals = []
+            list_countries = []
+            for iso, num_goals in goals.items():
+                list_iso.append(iso)
+                list_goals.append(num_goals)
+                list_countries.append(pycountry.countries.get(alpha_3=iso).name)
+            for country in pycountry.countries:
+                if country.alpha_3 not in blacklist:
+                    list_iso.append(country.alpha_3)
+                    list_goals.append(goals.get(country.alpha_3, 0))
+                    list_countries.append(country.name)
+            top5 = {}
+            for iso, goals in Counter(goals).most_common(5):
+                top5[pycountry.countries.get(alpha_3=iso).name] = goals
+        
+            data[str(year)] = (list_iso, list_goals, list_countries, top5)
+        
+        return data, None
+    else:
+        raise Exception('Not implemented by server')
