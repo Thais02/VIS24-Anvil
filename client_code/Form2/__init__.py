8a5461197f49c667ff4ab2e137d4084b638143cf
thijsblom2002@gmail.com <thijsblom2002@gmail.com>
2024-01-24 18:20:00 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -9,15 +9,44 @@ class Form2(Form2Template):
         self.dic = dic
         self.years, self.reds, self.yellows = cards_data
         self.country = country
+
+        for year_iter in range(1930, 2022 + 1, 4):
+            if year_iter not in self.years:
+                self.years.append(year_iter)
+                self.reds.append(0)
+                self.yellows.append(0)
+        
         self.init_components(**properties)
         self.update(year=year)
 
     def update(self, year=None):
+        index = None
         if year:
-            try:
-                index = self.years.index(year)
-            except ValueError:
-                index = None
+            if isinstance(year, int):
+                try:
+                    index = self.years.index(year)
+                except ValueError:
+                    index = None
+            else:
+                for y in year:
+                    index = []
+                    try:
+                        i = self.years.index(year)
+                        index.append(i)
+                    except ValueError:
+                        pass
+
+        selectedpoints = False
+        if year and index:
+            if isinstance(year, int):
+                if self.reds[index] or self.yellows[index]:
+                    selectedpoints = [index]
+            else:
+                for i in index:
+                    if self.reds[i] or self.yellows[i]:
+                        selectedpoints = index
+                        break
+            
 
         self.plot.layout = {'barmode': 'stack'}
         self.plot.layout.xaxis.tick0 = 1930
@@ -28,8 +57,8 @@ class Form2(Form2Template):
         self.plot.layout.title = "Red and yellow cards per year"
         
         self.plot.data = [
-            go.Bar(name='Red cards', x=self.years, y=self.reds, marker={'color': '#DA291C'}, selectedpoints=[index] if year and index else False),
-            go.Bar(name='Yellow cards', x=self.years, y=self.yellows, marker={'color': '#FFC72C'}, selectedpoints=[index] if year and index else False)
+            go.Bar(name='Red cards', x=self.years, y=self.reds, marker={'color': '#DA291C'}, selectedpoints=selectedpoints),
+            go.Bar(name='Yellow cards', x=self.years, y=self.yellows, marker={'color': '#FFC72C'}, selectedpoints=selectedpoints)
         ]
 
         self.richtext_side.content = f'|{self.country}|1930 - 2022|\n| --- | ---: |\n'
